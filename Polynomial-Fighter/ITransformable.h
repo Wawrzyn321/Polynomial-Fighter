7cfa6a1e65c14c9e56396964497c85bc45148d23
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2017-10-23 21:04:30 +0200

    Added examplary entities, awaiting more

--- Polynomial-Fighter/ITransformable.h
+++ Polynomial-Fighter/ITransformable.h
@@ -15,10 +15,11 @@ protected:
             (collisionRadius + other->getCollisionRadius())*(collisionRadius + other->getCollisionRadius());
     }
 public:
+	virtual ~ITransformable() = default;
 	bool collisionsEnabled = true;
 
 	virtual sf::Vector2f getPosition() = 0;
-	virtual void setPosition(sf::Vector2f position) = 0;
+	virtual void setPosition(const sf::Vector2f position) = 0;
     virtual float getCollisionRadius()
 	{
         return collisionRadius;
