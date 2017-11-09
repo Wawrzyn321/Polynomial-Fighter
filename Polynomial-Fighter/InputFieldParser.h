6bdb4ad84458885ce0d11a82d9c8c04b501a8a08
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2017-11-09 01:15:14 +0100

    Finished input parsing

--- Polynomial-Fighter/InputFieldParser.h
+++ Polynomial-Fighter/InputFieldParser.h
@@ -1,51 +1,13 @@
 #ifndef INPUT_FIELD_PARSER_H
 #define INPUT_FIELD_PARSER_H
-#include <vector>
 #include "Request.h"
-#include <iostream>
 
 class InputFieldParser
 {
-
-	static std::string preprocess(const std::string& input)
-	{
-		std::string unraw = input;
-		//remove ','
-		for (unsigned i = 0; i < unraw.size(); i++)
-		{
-			if (unraw[i] == ',') unraw[i] = ' ';
-		}
-		return unraw;
-	}
-
 public:
-	static std::vector<int> parse(const std::string &input)
-	{
-		std::string unraw = preprocess(input);
-		RequestHandler *chain =
-			new EmptyStringHandler(
-				new SingleNumberStringHandler(
-					new LHSInequalityStringHandler(
-						new RHSInequalityStringHandler(
-							new ListStringHandler(
-								nullptr)))));
-		Request r = chain->handle(unraw);
-		if (r.isValid())
-		{
-			for (int result : r.result)
-			{
-				std::cout << result << " ";
-			}
-			std::cout << std::endl;
-		}
-		else
-		{
-			std::cout << r.message << std::endl;
-		}
-		std::cout << std::endl;
-		delete chain;
-		return r.result;
-	}
+	static std::vector<int> parse(const std::string& input);
+
+	static void runTests();
 };
 
 
--- Polynomial-Fighter/Request.h
+++ Polynomial-Fighter/Request.h
@@ -3,17 +3,6 @@
 
 #include <string>
 #include <vector>
-class TEMPER
-{
-public:
-	static const int maxNumber = 7;
-
-	static bool isInRangeInclusive(int r)
-	{
-		return r >= -maxNumber && r <= maxNumber;
-	}
-
-};
 
 class Request
 {
@@ -25,83 +14,6 @@ public:
 	{
 		return message.empty();
 	}
-};
-
-class RequestHandler
-{
-protected:
-	RequestHandler *succesor = nullptr;
-
-	virtual bool canHandleRequest(const std::string &input) const = 0;
-
-	virtual Request handleImplementation(const std::string &input) const = 0;
-
-	Request passFurther(const std::string& input) const;
-public:
-	RequestHandler(RequestHandler* succesor);
-
-	Request handle(const std::string& input) const;
-
-	virtual ~RequestHandler();
-};
-
-class EmptyStringHandler : public RequestHandler
-{
-public:
-	EmptyStringHandler(RequestHandler *succesor) : RequestHandler(succesor) { }
-protected:
-	bool canHandleRequest(const std::string& input) const override;
-
-	Request handleImplementation(const std::string& input) const override;
-};
-
-class SingleNumberStringHandler : public RequestHandler
-{
-public:
-	SingleNumberStringHandler(RequestHandler *succesor) : RequestHandler(succesor) { }
-
-protected:
-	bool canHandleRequest(const std::string& input) const override;
-
-	Request handleImplementation(const std::string& input) const override;
-};
-
-class LHSInequalityStringHandler : public RequestHandler
-{
-public:
-	LHSInequalityStringHandler(RequestHandler *succesor) : RequestHandler(succesor) { }
-
-protected:
-	bool canHandleRequest(const std::string& input) const override;
-
-	Request handleImplementation(const std::string& input) const override;
-};
-
-
-class RHSInequalityStringHandler : public RequestHandler
-{
-public:
-	RHSInequalityStringHandler(RequestHandler *succesor) : RequestHandler(succesor) { }
-
-protected:
-	bool canHandleRequest(const std::string& input) const override;
-
-	Request handleImplementation(const std::string& input) const override;
-};
-
-
-class ListStringHandler : public RequestHandler
-{
-public:
-	ListStringHandler(RequestHandler *succesor) : RequestHandler(succesor) { }
-
-protected:
-	bool canHandleRequest(const std::string& input) const override;
-
-	Request handleImplementation(const std::string& input) const override;
-
-private:
-	static std::vector<std::string> tokenize(const std::string& input);
 };
 
 
