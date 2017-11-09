1bf8a16d16eb14d6cd6a33d08179e86d3311090b
Bartosz Ścigała <bscigala1995@gmail.com>
2017-11-09 21:33:50 +0100

    Fixed unsigned / signed bug.

--- Polynomial-Fighter/PolynomialGenerator.cpp
+++ Polynomial-Fighter/PolynomialGenerator.cpp
@@ -9,14 +9,14 @@ PolynomialProductForm PolynomialGenerator::generatePolynomial(unsigned int deg, 
         PolynomialFactor factor;
         do
         {
-            int x = 1;
+            int x;
             do
             {
-                x = RandomGenerator::getInt(-valuesRange, valuesRange);
+                x = RandomGenerator::getInt(-static_cast<int>(valuesRange), valuesRange);
             }
             while (x == 0);
             factor.setX(x);
-            factor.setConstant(RandomGenerator::getInt(-valuesRange, valuesRange));
+            factor.setConstant(RandomGenerator::getInt(-static_cast<int>(valuesRange), valuesRange));
         }
         while (!factor.isRootInteger());
         std::cout << "Wylosowany factor: x = " << factor.getX() << " const = " << factor.getConstant() << " root = " << factor.getRoot() << std::endl;
