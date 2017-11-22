ca5f6cdd40e5305361788a557d01548f79f6b18b
Bartosz Ścigała <bscigala1995@gmail.com>
2017-11-22 17:41:05 +0100

    Enchanced entity manager (better smart pointers usage, enabled/disabled awareness and added option to delete marked entities).

--- Polynomial-Fighter/Entity.h
+++ Polynomial-Fighter/Entity.h
@@ -10,27 +10,38 @@
 class Entity : public sf::Drawable, public ITransformable {
 protected:
     bool enabled = true;
+	bool toDelete = false;
 public:
     std::string name;
 	std::string tag;
 
-	Entity(const std::string &name = "", const std::string &tag ="") :
+	Entity(const std::string &name = "", const std::string &tag = "") :
 			name(name), tag(tag) {};
 
     virtual void update(Time::TimeData timeData) {};
 
     virtual void onDestroy() {};
 
-    virtual void setEnabled(bool enabled) {
+    void setEnabled(bool enabled)
+	{
         this->enabled = enabled;
     }
 
-	virtual bool getEnabled() {
+	bool getEnabled() const
+	{
 		return enabled;
 	}
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {};
 
+	bool getToDelete() const
+	{
+		return toDelete;
+	}
+	void setToDelete(bool toDelete)
+	{
+		this->toDelete = toDelete;
+	}
 };
 
 
